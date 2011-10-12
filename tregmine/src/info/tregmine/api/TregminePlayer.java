9490ca80072637b745b8b415f8bf7754e24a3668
ein andersson <einand@3gdev.com>
2011-10-13 00:58:55 +0200

    for some reason uid change to id in tregminplayer, its bug fixed now

--- tregmine/src/info/tregmine/api/TregminePlayer.java
+++ tregmine/src/info/tregmine/api/TregminePlayer.java
@@ -153,7 +153,7 @@ public class TregminePlayer extends PlayerDelegate
 	{
 		try {
 			this.mysql.connect();
-			String SQLD = "DELETE FROM `minecraft`.`user_settings` WHERE `user_settings`.`id` = "+ settings.get("uid") +" AND `user_settings`.`key` = '" + _key  +"'";
+			String SQLD = "DELETE FROM `minecraft`.`user_settings` WHERE `user_settings`.`id` = "+ settings.get("id") +" AND `user_settings`.`key` = '" + _key  +"'";
 			this.mysql.statement.execute(SQLD);
 
 			
