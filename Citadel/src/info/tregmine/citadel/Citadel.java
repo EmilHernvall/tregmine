50132467842f543ab3186f88a4d53623d570ea6e
ein andersson <einand@3gdev.com>
2011-10-13 04:09:15 +0200

    someone found out about citadel

--- Citadel/src/info/tregmine/citadel/Citadel.java
+++ Citadel/src/info/tregmine/citadel/Citadel.java
@@ -59,7 +59,7 @@ public class Citadel extends JavaPlugin {
 		info.tregmine.api.TregminePlayer tregPlayer = this.tregmine.tregminePlayer.get(from.getName());
 
 		if(commandName.equals("citadel") && tregPlayer.isDonator() ) {
-				from.teleport(this.getServer().getWorld("citadel").getSpawnLocation());
+				from.teleport(this.getServer().getWorld("citadel-").getSpawnLocation());
 		}
 		
 		return false;
