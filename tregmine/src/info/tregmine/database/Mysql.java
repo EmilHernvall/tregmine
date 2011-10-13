f210c2656a41e9718ae9e552ef07ab8b56330b30
ein andersson <einand@3gdev.com>
2011-10-13 19:08:38 +0200

    added propertys support to tregmine / mysql

--- tregmine/src/info/tregmine/database/Mysql.java
+++ tregmine/src/info/tregmine/database/Mysql.java
@@ -1,54 +1,69 @@
 package info.tregmine.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 //import java.sql.*;
 
 public class Mysql {
-	
+
 	public Connection connect = null;
 	public Statement statement = null;
-//	private PreparedStatement preparedStatement = null;
+	//	private PreparedStatement preparedStatement = null;
 	public ResultSet resultSet = null;
 
-	private String url = "jdbc:mysql://127.0.0.1/minecraft";
-	private String user = "minecraft";
-	private String pw = "cUqeWemUGeYaquxUpHaye8rUcrAteWre";
+	private String url = null; //"jdbc:mysql://127.0.0.1/minecraft";
+	private String user = null; // "minecraft";
+	private String pw = null; //"cUqeWemUGeYaquxUpHaye8rUcrAteWre";
 	//private String url = "jdbc:mysql://mc.tregmine.info:3306/minecraft"; 
 	//private String user = "minecraft"; 
 	//private String pw = "cUqeWemUGeYaquxUpHaye8rUcrAteWre";
 
 	// cUqeWemUGeYaquxUpHaye8rUcrAteWre
-	
-    public Mysql() {
-        	try {
-        		Class.forName("com.mysql.jdbc.Driver");
-        		
-        		
-    	    } catch (ClassNotFoundException ex) {
-    	    	ex.printStackTrace();
-    		}
-//       		this.connect();
-    }
-    
-    public void connect(){
-    	try {
-    		this.connect = DriverManager.getConnection( url, user, pw );
-    		this.statement = this.connect.createStatement();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    }
-    
-    public void close(){
-    	try {
-    		this.connect.close();
-    		this.statement.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    }
+
+	public Mysql() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		//       		this.connect();
+
+		Properties settings = new Properties();
+		try {
+			settings.load(new FileInputStream("mysql.cfg"));
+			this.url = settings.getProperty("url");
+			this.user = settings.getProperty("user");
+			this.pw = settings.getProperty("password");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void connect(){
+		try {
+			this.connect = DriverManager.getConnection( url, user, pw );
+			this.statement = this.connect.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close(){
+		try {
+			this.connect.close();
+			this.statement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
