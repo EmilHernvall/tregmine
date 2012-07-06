b40d88842d29d7cc123ff1ba586e3bb0cb40495e
ein andersson <einand@3gdev.com>
2012-07-06 23:34:16 +0200

    fixed data again

--- InventorySpy/src/info/tregmine/inventoryspy/SpyPlayerListener.java
+++ InventorySpy/src/info/tregmine/inventoryspy/SpyPlayerListener.java
@@ -48,7 +48,14 @@ public class SpyPlayerListener implements Listener {
 
 				for (int i = 0; i < invent.getSize(); i++) {
 					if (invent.getItem(i) != null) {
-						this.plugin.log.info("CHEST: " + "(" + loc.getBlockX() + "," + loc.getBlockY() + "," +  loc.getBlockZ() + ")" + "(" + i + ")" + "(" + event.getPlayer().getName() + ")" + invent.getItem(i).getType().toString()+  "(" + invent.getItem(i).getType().getId() + ")" + ":" + invent.getItem(i).getData() + " :: " + invent.getItem(i).getAmount());
+						this.plugin.log.info("CHEST: " + 
+								"(" + loc.getBlockX() + "," + loc.getBlockY() + "," +  loc.getBlockZ() + ")" + 
+								"(" + i + ")" + 
+								"(" + event.getPlayer().getName() + ")" 
+								+ invent.getItem(i).getType().toString() +  
+								"(" + invent.getItem(i).getType().getId() + ")" + 
+								":" + (byte) invent.getItem(i).getData().getData() + " :: " + 
+								invent.getItem(i).getAmount());
 					}
 				}
 
