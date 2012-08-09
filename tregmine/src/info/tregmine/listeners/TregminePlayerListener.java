aeaa72b209f3d72a3be29229e3df81e5cd59d31a
ein andersson <einand@3gdev.com>
2012-08-09 21:14:06 +0200

    fixed keyword

--- tregmine/src/info/tregmine/listeners/TregminePlayerListener.java
+++ tregmine/src/info/tregmine/listeners/TregminePlayerListener.java
@@ -249,7 +249,7 @@ public class TregminePlayerListener implements Listener {
 
  
 		if (tregPlayer.getMetaString("keyword") != null) {
-			String keyword = tregPlayer.getMetaString("keyword") + ".mc.tregmine.info:25565".toLowerCase();
+			String keyword = tregPlayer.getMetaString("keyword") + ".mc.tregmine.info".toLowerCase();
 			this.plugin.log.warning( event.getHostname() );
 			this.plugin.log.warning( keyword );
 
