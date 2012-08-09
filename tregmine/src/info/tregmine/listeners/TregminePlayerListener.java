89f51e4248a87fd9ae5b7920f70e23ab201f4804
ein andersson <einand@3gdev.com>
2012-08-09 21:07:14 +0200

    keyword enabled

--- tregmine/src/info/tregmine/listeners/TregminePlayerListener.java
+++ tregmine/src/info/tregmine/listeners/TregminePlayerListener.java
@@ -247,7 +247,7 @@ public class TregminePlayerListener implements Listener {
 		}
 
 
-
+ 
 		if (tregPlayer.getMetaString("keyword") != null) {
 			String keyword = tregPlayer.getMetaString("keyword") + ".mc.tregmine.info:25565".toLowerCase();
 			this.plugin.log.warning( event.getHostname() );
