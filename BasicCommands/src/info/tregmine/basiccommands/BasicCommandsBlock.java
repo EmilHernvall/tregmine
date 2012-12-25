c3be0c89d4da9e724bf8ffff24b4ae7a34faa288
ein andersson <einand@3gdev.com>
2012-12-25 22:01:12 +0100

    more debug text

--- BasicCommands/src/info/tregmine/basiccommands/BasicCommandsBlock.java
+++ BasicCommands/src/info/tregmine/basiccommands/BasicCommandsBlock.java
@@ -10,6 +10,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.hanging.HangingEvent;
+import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 //import org.bukkit.event.player.PlayerListener;
 
@@ -22,12 +25,9 @@ public class BasicCommandsBlock implements Listener {
 	}
 
 	@EventHandler 
-	public void onFrameHit(PlayerInteractEvent event) {
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+	public void onFrameHit(HangingPlaceEvent event) {
 			Player player = event.getPlayer();
-			Block block = event.getClickedBlock();
-			player.sendMessage("" + block.hashCode());
-		}
+			player.sendMessage("" + event.getBlock().hashCode());
 		
 	}
 	
