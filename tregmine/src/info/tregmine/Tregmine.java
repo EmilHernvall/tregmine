a1545f90679a3c91dd543e358eb1c2697f02cb8c
ein andersson <einand@3gdev.com>
2013-01-12 05:41:32 +0100

    spawn head

--- tregmine/src/info/tregmine/Tregmine.java
+++ tregmine/src/info/tregmine/Tregmine.java
@@ -146,12 +146,12 @@ public class Tregmine extends JavaPlugin
 		}
 
 		if(commandName.equals("head") && player.isAdmin()) {
-			ItemStack item = new ItemStack(Material.SKULL_ITEM, 1);
-			item.getData().setData((byte)3);
-			
+			ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
 			SkullMeta meta = (SkullMeta) item.getItemMeta();
 			meta.setOwner(args[0]);
 			meta.setDisplayName(args[0]);
+			item.setItemMeta(meta);
+			
 			PlayerInventory inv = player.getInventory();
 			inv.addItem(item);
 			player.updateInventory();
