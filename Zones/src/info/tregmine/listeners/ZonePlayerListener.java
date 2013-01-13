677583c46bf843b5b1b0774522b548296922c72c
ein andersson <einand@3gdev.com>
2013-01-14 00:41:32 +0100

    fixed zone spam

--- Zones/src/info/tregmine/listeners/ZonePlayerListener.java
+++ Zones/src/info/tregmine/listeners/ZonePlayerListener.java
@@ -550,30 +550,15 @@ public class ZonePlayerListener implements Listener
 	{
 		player.sendMessage(ChatColor.RED + "[" + currentZone.getName() + "] " + 
 				currentZone.getTextEnter());
-		
-		if (this.plugin.pvp.containsKey(player.getName())) {
-			this.plugin.pvp.put(player.getName(), currentZone.isPvp());
-		}
-
-		
 		if (currentZone.isPvp()) {
 			player.sendMessage(ChatColor.RED + "[" + currentZone.getName() + "] " + 
 					"Warning! This is a PVP zone! Other players can damage or kill you here.");
-			
 			if (this.plugin.pvp.get(player.getName())) {
 				player.setTexturePack("https://dl.dropbox.com/u/5405236/mc/pvp.zip");
 			}
-			
-			
 		} else {
-			
-			if (!this.plugin.pvp.get(player.getName())) {
-				player.setTexturePack("https://dl.dropbox.com/u/5405236/mc/df.zip");
-			}
+			player.setTexturePack("https://dl.dropbox.com/u/5405236/mc/df.zip");
 		}
-		
-		this.plugin.pvp.put(player.getName(), currentZone.isPvp());
-		
 		if (perm != null) {
 			String permNotification = perm.getPermissionNotification();
 			player.sendMessage(ChatColor.RED + "[" + currentZone.getName() + "] " + 
