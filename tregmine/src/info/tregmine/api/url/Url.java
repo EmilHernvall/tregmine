554f49d99864d4d51ee7196c3f16f4af101b3499
ein andersson <einand@3gdev.com>
2013-01-15 00:46:56 +0100

    url shortner

--- tregmine/src/info/tregmine/api/url/Url.java
+++ tregmine/src/info/tregmine/api/url/Url.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Url {
 
 	public static String getURL(String _text){
-		String regex = "<\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>";
+		String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(_text);
