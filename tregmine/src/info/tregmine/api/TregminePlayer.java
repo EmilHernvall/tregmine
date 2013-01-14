29288f758f44714426363359a19d5d059a9bb7eb
ein andersson <einand@3gdev.com>
2013-01-14 20:15:41 +0100

    removed sendtexture

--- tregmine/src/info/tregmine/api/TregminePlayer.java
+++ tregmine/src/info/tregmine/api/TregminePlayer.java
@@ -262,11 +262,11 @@ public class TregminePlayer extends PlayerDelegate
 			System.out.print(stmt.getWarnings().toString());
 
 			System.out.print(stmt.getWarnings().toString());
-			
+
 			System.out.print(settings.get("uid"));
 			System.out.print(_key);
 			System.out.print(sqlDelete);
-			
+
 			stmt.close();
 			stmt = null;
 
@@ -475,27 +475,16 @@ public class TregminePlayer extends PlayerDelegate
 		}
 	}
 
-	public void resendTexture() {
-
-		if(this.getMetaString("text") == null) {
-			this.setMetaString("text", "https://dl.dropbox.com/u/5405236/mc/df.zip");
-			this.setTexturePack("https://dl.dropbox.com/u/5405236/mc/df.zip");
-		} else {
-			this.setTexturePack(this.getMetaString("text"));
-		}
-
-	}
 
 	public void setCurrentTexture(String _url) {
-
-		if(this.getMetaString("text") == null) {
-			this.setMetaString("text", "https://dl.dropbox.com/u/5405236/mc/df.zip");
-			this.setTexturePack("https://dl.dropbox.com/u/5405236/mc/df.zip");
-		} else 	if (!this.getMetaString("text").matches(_url)) {
-			this.setMetaString("text", _url);
-			this.setTexturePack(_url);
+//		if(this.getMetaString("text") == null) {
+//			this.setMetaString("text", "https://dl.dropbox.com/u/5405236/mc/df.zip");
+//			this.setTexturePack("https://dl.dropbox.com/u/5405236/mc/df.zip");
+//		} else 	if (!this.getMetaString("text").matches(_url)) {
+//			this.setMetaString("text", _url);
+//			this.setTexturePack(_url);
 		}
-	}
+
 
 	public void setChatChannel(String _channel) {
 		this.setMetaString("channel", _channel.toUpperCase());
