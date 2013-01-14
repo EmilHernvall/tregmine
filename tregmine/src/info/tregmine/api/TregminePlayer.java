461f05191a9f950883609d45ed65128c3481febe
ein andersson <einand@3gdev.com>
2013-01-14 20:30:08 +0100

    added value null to ""

--- tregmine/src/info/tregmine/api/TregminePlayer.java
+++ tregmine/src/info/tregmine/api/TregminePlayer.java
@@ -247,13 +247,19 @@ public class TregminePlayer extends PlayerDelegate
 	public void setMetaString(String _key, String _value) 	{
 		Connection conn = null;
 		PreparedStatement stmt = null;
+		if (_value == null) {
+			_value = "";
+		}
+		
 		try {
 			conn = ConnectionPool.getConnection();
 
 			String sqlDelete = "DELETE FROM `minecraft`.`user_settings` " +
 					"WHERE `user_settings`.`id` = ? AND `user_settings`.`key` = ?";
 			stmt = conn.prepareStatement(sqlDelete);
+			
 			System.out.print(stmt.getWarnings().toString());
+			
 			stmt.setString(1, settings.get("uid"));
 			System.out.print(stmt.getWarnings().toString());
 			stmt.setString(2, _key);
