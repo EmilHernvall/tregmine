b50a84ff1b97873474e4f021bb1c306ce7679af2
ein andersson <einand@3gdev.com>
2013-01-15 00:45:08 +0100

    added the missing fukes

---
+++ tregmine/src/info/tregmine/api/url/Url.java
@@ -0,0 +1,27 @@
+package info.tregmine.api.url;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Url {
+
+	public static String getURL(String _text){
+		String regex = "<\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>";
+
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(_text);
+
+		String g = null;
+
+		while (m.find()) {
+			g = m.group(0);
+		}
+
+
+		return g;
+	}
+
+
+
+}
