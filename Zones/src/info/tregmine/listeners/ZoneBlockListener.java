2a162fc35a30a861bed6a5d293f081c792791f04
Ein Andersson <einand@3gdev.com>
2013-01-18 22:21:52 +0100

    updated to work only with paper

--- Zones/src/info/tregmine/listeners/ZoneBlockListener.java
+++ Zones/src/info/tregmine/listeners/ZoneBlockListener.java
@@ -61,12 +61,12 @@ public class ZoneBlockListener implements Listener
 		}
 
 
+		if (event.getPlayer().getItemInHand().equals(Material.PAPER)) {
 			ItemStack paper = player.getItemInHand();
 			ItemMeta papermeta = paper.getItemMeta();
 			player.sendMessage("NAME: " + paper.getType().toString());
 
 			
-			if (event.getPlayer().getItemInHand().equals(Material.PAPER)) {
 			if (papermeta.hasDisplayName()) {
 				player.sendMessage("NAME: " + papermeta.getDisplayName());
 			}
