813de49a1dce887d86d8db33a3848eea6db62117
Ein Andersson <einand@3gdev.com>
2013-01-18 16:33:38 +0100

    sign test

--- Zones/src/info/tregmine/listeners/ZonePlayerListener.java
+++ Zones/src/info/tregmine/listeners/ZonePlayerListener.java
@@ -259,7 +259,7 @@ public class ZonePlayerListener implements Listener
 //			Zone zone = world.findZone(currentPos);
 			Lot lot = world.findLot(currentPos);
 			
-			if ("for sale".equals(block.getLine(1).toLowerCase())) {
+			if (block.getLine(0).toLowerCase().contains("forsale")) {
 				player.sendMessage("" + lot.getName());				
 			}
 			
