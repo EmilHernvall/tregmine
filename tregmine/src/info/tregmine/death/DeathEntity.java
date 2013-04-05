a0c606432afbc200f39ff07c3e9161c305bf0db2
Ein Andersson <einand@3gdev.com>
2013-04-05 22:17:39 +0200

    made it so you drop your head if you die

--- tregmine/src/info/tregmine/death/DeathEntity.java
+++ tregmine/src/info/tregmine/death/DeathEntity.java
@@ -5,6 +5,7 @@ package info.tregmine.death;
 import info.tregmine.Tregmine;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +13,8 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public class DeathEntity implements Listener  {
 	private final Tregmine plugin;
@@ -27,6 +30,15 @@ public class DeathEntity implements Listener  {
 		 if (event instanceof PlayerDeathEvent) {
 			 Player player = (Player) event.getEntity();
              PlayerDeathEvent e = (PlayerDeathEvent) event;
+             
+             
+ 			ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
+ 			SkullMeta meta = (SkullMeta) item.getItemMeta();
+ 			meta.setOwner(player.getName());
+ 			meta.setDisplayName(ChatColor.GRAY + player.getName());
+ 			item.setItemMeta(meta);
+            player.getWorld().dropItemNaturally(player.getLocation(), item);
+             
              e.setDeathMessage(ChatColor.DARK_GRAY + "DIED - " + player.getName() + " " + Insult.random());
          }
 	}
