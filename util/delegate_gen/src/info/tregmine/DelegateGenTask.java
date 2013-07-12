46083b61165dbf59284a9cff210cc5b26b4111df
Emil Hernvall <emil@quench.at>
2013-07-12 17:47:08 +0200

    Added tool for automatically generating PlayerDelegate

---
+++ util/delegate_gen/src/info/tregmine/DelegateGenTask.java
@@ -0,0 +1,61 @@
+package info.tregmine;
+
+import java.io.*;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.DirectoryScanner;
+
+public class DelegateGenTask extends Task
+{
+    public String srcInterface;
+    public String dstClass;
+    public Path outputDir;
+
+    public DelegateGenTask()
+    {
+    }
+
+    @Override
+    public void execute()
+    throws BuildException
+    {
+        System.out.println("Generating " + dstClass);
+
+        String[] outDirs = outputDir.list();
+        if (outDirs.length != 1) {
+            throw new BuildException("Only one output dir may be specified.");
+        }
+
+        File outDir = new File(outDirs[0]);
+
+        String subPath = dstClass.replaceAll("\\.", File.separator) + ".java";
+        File targetFile = new File(outDir, subPath);
+
+        try {
+            PrintStream stream = new PrintStream(targetFile);
+            DelegateGen.generateDelegate(stream, srcInterface, dstClass);
+            stream.close();
+        }
+        catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+    public void setSourceinterface(String srcInterface)
+    {
+        this.srcInterface = srcInterface;
+    }
+
+    public void setDestinationclass(String dstClass)
+    {
+        this.dstClass = dstClass;
+    }
+
+    public void setOutputdir(Path path)
+    {
+        this.outputDir = path;
+    }
+}
