ed92391187bb4de82ea01c3954cf8c3d591271c6
Emil Hernvall <emil@znaptag.com>
2013-08-18 01:38:34 +0200

    Added missing imports

--- src/info/tregmine/listeners/TregmineBlockListener.java
+++ src/info/tregmine/listeners/TregmineBlockListener.java
@@ -5,11 +5,15 @@ import java.util.EnumSet;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
