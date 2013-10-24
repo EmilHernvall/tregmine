2a4d9b5dfbf3fb8061f1fd851ddf0523a184a208
Josh Morgan <jm109876543210@gmail.com>
2013-10-25 08:24:52 +1100

    Update HeadCommand.java
    To stay in style with other commands

--- src/info/tregmine/commands/HeadCommand.java
+++ src/info/tregmine/commands/HeadCommand.java
@@ -11,31 +11,33 @@ import info.tregmine.api.TregminePlayer;
 
 public class HeadCommand extends AbstractCommand
 {
-    public HeadCommand(Tregmine tregmine)
-    {
-        super(tregmine, "head");
-    }
+	public HeadCommand(Tregmine tregmine)
+	{
+		super(tregmine, "head");
+	}
 
-    @Override
-    public boolean handlePlayer(TregminePlayer player, String args[])
-    {
-        if (player.getRank().canGetPlayerHead()){
-            if (args.length == 1) {
-                ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
-                SkullMeta itemMeta = (SkullMeta) item.getItemMeta();
-                itemMeta.setOwner(args[0]);
-                itemMeta.setDisplayName(ChatColor.YELLOW + args[0] + "'s head");
-                item.setItemMeta(itemMeta);
-                PlayerInventory inventory = player.getInventory();
-                inventory.addItem(item);
-                player.updateInventory();
-                player.sendMessage(ChatColor.YELLOW + "You received the head of " + args[0]);
-            } else {
-                player.sendMessage(ChatColor.RED + "Type /head <player>");
-            }
-        } else {
-            player.sendMessage(ChatColor.RED + "Only admins can use this command!");
-        }
-        return true;
-    }
+	@Override
+	public boolean handlePlayer(TregminePlayer player, String args[])
+	{
+
+		if (args.length != 1) {
+			return false;
+		}
+
+		if (!player.getRank().canGetPlayerHead()) {
+			return true;
+		}
+
+		ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
+		SkullMeta itemMeta = (SkullMeta) item.getItemMeta();
+		itemMeta.setOwner(args[0]);
+		itemMeta.setDisplayName(ChatColor.YELLOW + args[0] + "'s head");
+		item.setItemMeta(itemMeta);
+		PlayerInventory inventory = player.getInventory();
+		inventory.addItem(item);
+		player.updateInventory();
+		player.sendMessage(ChatColor.YELLOW + "You received the head of " + args[0]);
+
+		return true;
+	}
 }
