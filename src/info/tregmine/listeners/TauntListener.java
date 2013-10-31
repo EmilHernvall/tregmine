11c9cea6872cd01cbc03fb3f7780346e08745955
lDiverse <lDiverse@users.noreply.github.com>
2013-10-31 12:38:06 +0000

    Zombie Spawn with Player head (100% drop)

--- src/info/tregmine/listeners/TauntListener.java
+++ src/info/tregmine/listeners/TauntListener.java
@@ -7,13 +7,16 @@ import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
-//import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import info.tregmine.Tregmine;
@@ -29,33 +32,41 @@ public class TauntListener implements Listener
     }
 
     @EventHandler
-    public void onEntityDeath(EntityDeathEvent event)
-    {
+    public void onDeath(PlayerDeathEvent event){
+        
         if (!(event instanceof PlayerDeathEvent)) {
             return;
         }
 
-        Player player = (Player) event.getEntity();
+        Player p = (Player) event.getEntity();
         PlayerDeathEvent e = (PlayerDeathEvent) event;
 
         String death =
-                ChatColor.DARK_GRAY + "DIED - " + player.getName() + " "
+                ChatColor.DARK_GRAY + "DIED - " + p.getName() + " "
                         + Insult.random();
-
+        
+        World w = p.getWorld();
+        
+        Entity a = w.spawnEntity(p.getLocation(), EntityType.ZOMBIE);
+        if(!(a instanceof Zombie)) return;
+        
         ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
         SkullMeta meta = (SkullMeta) item.getItemMeta();
-        meta.setOwner(player.getName());
-        meta.setDisplayName(ChatColor.GRAY + player.getName());
-
+        meta.setOwner(p.getName());
+        meta.setDisplayName(ChatColor.GRAY + p.getName());
         List<String> lore = new ArrayList<String>();
-        lore.add(ChatColor.stripColor(e.getDeathMessage()));
-
+        lore.add(ChatColor.stripColor(event.getDeathMessage()));
         meta.setLore(lore);
-
         item.setItemMeta(meta);
-
-        player.getWorld().dropItemNaturally(player.getLocation(), item);
-
-        e.setDeathMessage(death);
+        
+        Zombie zomb = (Zombie) a;
+        zomb.setCustomName(p.getDisplayName());
+        zomb.setCustomNameVisible(true);
+        
+        EntityEquipment ee = zomb.getEquipment();
+        ee.setHelmet(item);
+        ee.setHelmetDropChance(1F);
+        
+        zomb.setCanPickupItems(false);
     }
 }
