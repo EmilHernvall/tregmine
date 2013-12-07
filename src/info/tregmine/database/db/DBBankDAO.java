030ac9a525fd9167b07cb604f23617439190f81b
Sabersamus <rcatron10@gmail.com>
2013-12-07 12:17:54 -0800

    Added deposit/withdraw support to accounts

--- src/info/tregmine/database/db/DBBankDAO.java
+++ src/info/tregmine/database/db/DBBankDAO.java
@@ -15,9 +15,9 @@ import info.tregmine.database.IBankDAO;
 
 public class DBBankDAO implements IBankDAO
 {
-    
+
     private Connection conn;
-    
+
     public DBBankDAO(Connection conn)
     {
         this.conn = conn;
@@ -29,45 +29,44 @@ public class DBBankDAO implements IBankDAO
     {
         String sql = "SELECT * FROM banks WHERE bank_name = ? LIMIT 1";
         Bank bank = null;
-        try(PreparedStatement stm = conn.prepareStatement(sql)){
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
             stm.setString(1, name);
             stm.execute();
-            
+
             ResultSet rs = stm.getResultSet();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 bank = new Bank(name);
                 bank.setId(rs.getInt("bank_id"));
                 bank.setAccounts(this.getAccounts(bank));
                 bank.setLotId(rs.getInt("lot_id"));
-                
+
                 return bank;
             }
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             throw new DAOException(sql, e);
         }
         return null;
     }
 
     @Override
-    public int createBank(Bank bank)
-    throws DAOException
+    public int createBank(Bank bank) throws DAOException
     {
         String sql = "INSERT INTO banks (bank_name, lot_id) VALUES (?, ?)";
-        try(PreparedStatement stm = conn.prepareStatement(sql)){
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
             stm.setString(1, bank.getName());
             stm.setInt(2, bank.getLotId());
             stm.execute();
-            
-            try(ResultSet rs = stm.getResultSet()){
-                if(!rs.next()){
+
+            try (ResultSet rs = stm.getResultSet()) {
+                if (!rs.next()) {
                     return 0;
                 }
                 return rs.getInt("bank_id");
             }
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             throw new DAOException(sql, e);
         }
     }
@@ -78,51 +77,84 @@ public class DBBankDAO implements IBankDAO
     {
         String sql = "SELECT * FROM bank_accounts WHERE bank_name = ?";
         List<Account> accounts = Lists.newArrayList();
-        try(PreparedStatement stm = conn.prepareStatement(sql)){
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
             stm.setString(1, bank.getName());
             stm.execute();
-            
-            try(ResultSet rs = stm.getResultSet()){
-            
-                while(rs.next()){
+
+            try (ResultSet rs = stm.getResultSet()) {
+
+                while (rs.next()) {
                     Account acct = new Account();
                     acct.setBank(bank);
                     acct.setPlayer(rs.getString("player_name"));
-                    acct.setAmount(rs.getLong("account_balance"));
+                    acct.setBalance(rs.getLong("account_balance"));
                     accounts.add(acct);
                 }
-            
+
             }
             return accounts;
-            
+
+        } catch (SQLException e) {
+            throw new DAOException(sql, e);
+        }
+    }
+
+    @Override
+    public Account getAccount(Bank bank, String player) 
+    throws DAOException
+    {
+        String sql =
+                "SELECT * FROM bank_accounts WHERE bank_name = ? AND player_name = ?";
+        Account acct = null;
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+            stm.setInt(1, bank.getId());
+            stm.setString(2, player);
+            stm.execute();
+
+            ResultSet rs = stm.getResultSet();
+            if (rs.next()) {
+                acct = new Account();
+                acct.setBank(bank);
+                acct.setPlayer(player);
+                acct.setBalance(rs.getLong("account_balance"));
+                return acct;
+            }
+        } catch (SQLException e) {
+            throw new DAOException(sql, e);
+        }
+        return null;
+    }
+
+    @Override
+    public void deposit(Bank bank, Account acct, long amount)
+    throws DAOException
+    {
+        String sql = "UPDATE bank_accounts SET account_balance = ? WHERE bank_name = ? AND player_name = ?";
+        try(PreparedStatement stm = conn.prepareStatement(sql)){
+            stm.setLong(1, acct.getBalance() + amount);
+            stm.setString(2, bank.getName());
+            stm.setString(3, acct.getPlayer());
+            stm.execute();
         }catch(SQLException e){
             throw new DAOException(sql, e);
         }
     }
 
     @Override
-    public Account getAccount(Bank bank, String player)
+    public boolean withdraw(Bank bank, Account acct, long amount)
     throws DAOException
     {
-        String sql = "SELECT * FROM bank_accounts WHERE bank_name = ? AND player_name = ?";
-        Account acct = null;
+        if(acct.getBalance() - amount < 0)return false;
+        String sql = "UPDATE bank_accounts SET account_balance = ? WHERE bank_name = ? AND player_name = ?";
         try(PreparedStatement stm = conn.prepareStatement(sql)){
-            stm.setInt(1, bank.getId());
-            stm.setString(2, player);
+            stm.setLong(1, acct.getBalance() - amount);
+            stm.setString(2, bank.getName());
+            stm.setString(3, acct.getPlayer());
             stm.execute();
-            
-            ResultSet rs = stm.getResultSet();
-            if(rs.next()){
-                acct = new Account();
-                acct.setBank(bank);
-                acct.setPlayer(player);
-                acct.setAmount(rs.getLong("account_balance"));
-                return acct;
-            }
         }catch(SQLException e){
             throw new DAOException(sql, e);
         }
-        return null;
+        return true;
     }
 
 }
