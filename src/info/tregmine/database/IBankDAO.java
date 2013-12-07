4dfc4704601f664cd48e198f6bddaa0bc5b66f35
Sabersamus <rcatron10@gmail.com>
2013-12-07 03:03:37 -0800

    Bank work

---
+++ src/info/tregmine/database/IBankDAO.java
@@ -0,0 +1,19 @@
+package info.tregmine.database;
+
+import java.util.List;
+
+import info.tregmine.api.Account;
+import info.tregmine.api.Bank;
+
+public interface IBankDAO
+{
+    
+    public Bank getBank(String name) throws DAOException;
+    public Bank createBank(Bank bank) throws DAOException;
+    
+    public List<Account> getAccounts(Bank bank) throws DAOException;
+    public Account getAccount(Bank bank, String player) throws DAOException;
+    
+    
+    
+}
