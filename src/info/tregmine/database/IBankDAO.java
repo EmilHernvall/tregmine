982b0ec89f067616be5be2c09451c3e18c291376
Sabersamus <rcatron10@gmail.com>
2013-12-07 11:47:23 -0800

    Finished (I think) work onDBBankDAO

--- src/info/tregmine/database/IBankDAO.java
+++ src/info/tregmine/database/IBankDAO.java
@@ -9,7 +9,7 @@ public interface IBankDAO
 {
     
     public Bank getBank(String name) throws DAOException;
-    public Bank createBank(Bank bank) throws DAOException;
+    public int createBank(Bank bank) throws DAOException;
     
     public List<Account> getAccounts(Bank bank) throws DAOException;
     public Account getAccount(Bank bank, String player) throws DAOException;
