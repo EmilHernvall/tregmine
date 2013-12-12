34f70a74e9f9002ae9b4236cbabc6037b6a9aca8
James Sherlock <lDiverse@users.noreply.github.com>
2013-12-12 14:13:05 +0000

    New InventoryCommand using up to date system.

--- src/info/tregmine/commands/InventoryCommand.java
+++ src/info/tregmine/commands/InventoryCommand.java
@@ -2,6 +2,8 @@ package info.tregmine.commands;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
+
 import info.tregmine.Tregmine;
 import info.tregmine.api.TregminePlayer;
 
@@ -16,21 +18,48 @@ public class InventoryCommand extends AbstractCommand
     public boolean handlePlayer(TregminePlayer player, String[] args)
     {
         if (!player.getRank().canInspectInventories()) {
-            return false;
+            return true;
         }
-        if (args.length == 0) {
-            return false;
+        
+        if ("save".equalsIgnoreCase(args[0]) && args.length == 1) {
+            player.saveInventory(player.getCurrentInventory());
+            return true;
         }
-
-        List<TregminePlayer> candidates = tregmine.matchPlayer(args[0]);
-        if (candidates.size() != 1) {
-            // TODO: error message
-            return false;
+        
+        if ("reload".equalsIgnoreCase(args[0]) && args.length == 3) {
+            List<TregminePlayer> candidates = tregmine.matchPlayer(args[1]);
+            if (candidates.size() != 1) {
+                player.sendMessage(ChatColor.RED + "Player: " + args[1] + " not found!");
+                return true;
+            }
+            TregminePlayer candidate = candidates.get(0);
+            boolean state = "true".equalsIgnoreCase(args[2]);
+            
+            if (state) {
+                candidate.loadInventory(candidate.getCurrentInventory(), true);
+            } else {
+                candidate.loadInventory(candidate.getCurrentInventory(), false);
+            }
+            player.sendMessage(ChatColor.GREEN + "Reloaded " + candidate.getChatName() + "'s inventory from DB!");
+            return true;
         }
-
-        TregminePlayer candidate = candidates.get(0);
-        player.openInventory(candidate.getInventory());
-
+        
+        if ("inspect".equalsIgnoreCase(args[0]) && args.length == 2) {
+            List<TregminePlayer> candidates = tregmine.matchPlayer(args[1]);
+            if (candidates.size() != 1) {
+                player.sendMessage(ChatColor.RED + "Player: " + args[1] + " not found!");
+                return true;
+            }
+            TregminePlayer candidate = candidates.get(0);
+            player.openInventory(candidate.getInventory());
+            player.sendMessage(ChatColor.GREEN + "Inspecting " + candidate.getChatName() + "'s inventory!");
+            return true;
+        }
+        
+        player.sendMessage(ChatColor.RED + "Incorrect usage:");
+        player.sendMessage(ChatColor.AQUA + "/inv inspect <name> - Inspect someones inventory");
+        player.sendMessage(ChatColor.AQUA + "/inv reload <name> <true/false> - Reload inventory, optional save");
+        player.sendMessage(ChatColor.AQUA + "/inv save - Save your current inventory to database");
         return true;
     }
 }
