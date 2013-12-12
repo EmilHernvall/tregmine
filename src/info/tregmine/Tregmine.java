40145736d9206d7726b532b9d903262a6ac04597
James Sherlock <lDiverse@users.noreply.github.com>
2013-12-12 14:15:17 +0000

    Added saving of inventories on disable

--- src/info/tregmine/Tregmine.java
+++ src/info/tregmine/Tregmine.java
@@ -50,7 +50,6 @@ public class Tregmine extends JavaPlugin
     private Map<Integer, TregminePlayer> playersById;
     private Map<Location, Integer> blessedBlocks;
     private Map<Location, FishyBlock> fishyBlocks;
-    private Map<Integer, Tank> liquidTanks;
 
     private Map<String, ZoneWorld> worlds;
     private Map<Integer, Zone> zones;
@@ -128,7 +127,6 @@ public class Tregmine extends JavaPlugin
             this.fishyBlocks = fishyBlockDAO.loadFishyBlocks(getServer());
 
             LOGGER.info("Loaded " + fishyBlocks.size() + " fishy blocks");
-            
         } catch (DAOException e) {
             throw new RuntimeException(e);
         }
@@ -163,7 +161,6 @@ public class Tregmine extends JavaPlugin
         pluginMgm.registerEvents(new ToolCraft(this), this);
         pluginMgm.registerEvents(new LumberListener(this), this);
         pluginMgm.registerEvents(new VeinListener(this), this);
-        pluginMgm.registerEvents(new PortalListener(this), this);
 
         // Declaration of all commands
         getCommand("admins").setExecutor(
@@ -594,11 +591,6 @@ public class Tregmine extends JavaPlugin
     public Zone getZone(int zoneId)
     {
         return zones.get(zoneId);
-    }
-    
-    public Map<Integer, Tank> getTanks()
-    {
-        return liquidTanks;
     }
 
     // ============================================================================
