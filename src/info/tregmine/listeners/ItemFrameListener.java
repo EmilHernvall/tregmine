6200d92da0bbb8fd6ac6912339dc6f7659ce447f
James Sherlock <lDiverse@users.noreply.github.com>
2013-12-16 13:17:59 +0000

    Fix's itemframes dropping items on left click without valid permission.
    Spoke to a BukkitDev and they told me to listen for this event, did some testing and boom. Works.

--- src/info/tregmine/listeners/ItemFrameListener.java
+++ src/info/tregmine/listeners/ItemFrameListener.java
@@ -5,8 +5,11 @@ import info.tregmine.api.TregminePlayer;
 
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class ItemFrameListener implements Listener
@@ -30,6 +33,24 @@ public class ItemFrameListener implements Listener
         Location location = event.getRightClicked().getLocation();
         
         if (!player.hasBlockPermission(location, true)) {
+            event.setCancelled(true);
+        }
+    }
+    
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event)
+    {
+        if (!(event.getEntity() instanceof ItemFrame)) {
+            return;
+        }
+        
+        if (!(event.getDamager() instanceof Player)) {
+            return;
+        }
+        
+        TregminePlayer player = plugin.getPlayer((Player) event.getDamager());
+        
+        if (!player.hasBlockPermission(event.getEntity().getLocation(), true)) {
             event.setCancelled(true);
         }
     }
