2fc5d5b62752d58624d209f913f3e5ce52cd5763
Emil Hernvall <emil@quench.at>
2013-12-16 14:00:59 +0100

    Fixed compile error

--- src/info/tregmine/listeners/TauntListener.java
+++ src/info/tregmine/listeners/TauntListener.java
@@ -2,13 +2,6 @@ package info.tregmine.listeners;
 
 //import java.util.Random;
 
-import info.tregmine.Tregmine;
-import info.tregmine.api.Insult;
-import info.tregmine.api.TregminePlayer;
-import info.tregmine.quadtree.Point;
-import info.tregmine.zones.Zone;
-import info.tregmine.zones.ZoneWorld;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +24,14 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 //import org.bukkit.inventory.meta.ItemMeta;
 
+import info.tregmine.Tregmine;
+import info.tregmine.api.Insult;
+import info.tregmine.api.TregminePlayer;
+import info.tregmine.quadtree.Point;
+import info.tregmine.zones.Lot;
+import info.tregmine.zones.Zone;
+import info.tregmine.zones.ZoneWorld;
+
 public class TauntListener implements Listener
 {
     private Tregmine plugin;
@@ -50,70 +51,71 @@ public class TauntListener implements Listener
         Player player = (Player) event.getEntity();
         PlayerDeathEvent e = (PlayerDeathEvent) event;
 
-        String death =
-                ChatColor.DARK_GRAY + "DIED - " + player.getName() + " "
-                        + Insult.random();
-        
+        String death = ChatColor.DARK_GRAY + "DIED - " + player.getName() +
+                       " " + Insult.random();
+
         EntityDamageEvent damage = player.getLastDamageCause();
         DamageCause cause = damage.getCause();
-        
+
         boolean playerCause = false;
         if (damage.getEntity() instanceof Player) playerCause = true;
-        
+
         Location location = player.getLocation();
-        
+
         Point pos = new Point(location.getBlockX(), location.getBlockZ());
         ZoneWorld world = plugin.getWorld(player.getWorld());
         TregminePlayer player2 = plugin.getPlayer(player);
-        
+
         Zone currentZone = player2.getCurrentZone();
         if (currentZone == null || !currentZone.contains(pos)) {
             currentZone = world.findZone(pos);
             player2.setCurrentZone(currentZone);
         }
-        
+
         if (currentZone == null) {
             e.setDeathMessage(death);
             return;
         }
-        
+
         boolean lotPVP = false;
         Lot potentialLot = world.findLot(location);
-        if (lot != null &&
+        if (potentialLot != null &&
             potentialLot.hasFlag(Lot.Flags.PVP)) {
             lotPVP = true;
         }
-        
+
         if (cause == DamageCause.ENTITY_ATTACK &&
-                (currentZone.isPvp() || lotPVP == true) &&
+                (currentZone.isPvp() || lotPVP) &&
                 playerCause == true) {
-            
+
             World w = player.getWorld();
             Entity a = w.spawnEntity(player.getLocation(), EntityType.ZOMBIE);
-            if(!(a instanceof Zombie)) return;
-            
+            if (!(a instanceof Zombie)) {
+                return;
+            }
+
             ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
             SkullMeta meta = (SkullMeta) item.getItemMeta();
             meta.setOwner(player.getName());
             meta.setDisplayName(ChatColor.GRAY + player.getName());
-    
+
             List<String> lore = new ArrayList<String>();
             lore.add(ChatColor.stripColor(e.getDeathMessage()));
             meta.setLore(lore);
-    
+
             item.setItemMeta(meta);
-            
+
             Zombie zomb = (Zombie) a;
             zomb.setCustomName(player.getDisplayName());
             zomb.setCustomNameVisible(true);
-            
+
             EntityEquipment ee = zomb.getEquipment();
             ee.setHelmet(item);
             ee.setHelmetDropChance(1F);
-            
+
             zomb.setCanPickupItems(false);
         }
-        
+
         e.setDeathMessage(death);
     }
 }
