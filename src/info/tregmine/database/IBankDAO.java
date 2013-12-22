ce2697ee31d4272e86a37581fd9b2f7163c97b1f
Sabersamus <rcatron10@gmail.com>
2013-12-21 18:25:46 -0800

    Implemented Pin numbers

--- src/info/tregmine/database/IBankDAO.java
+++ src/info/tregmine/database/IBankDAO.java
@@ -15,6 +15,7 @@ public interface IBankDAO
     public List<Account> getAccounts(Bank bank) throws DAOException;
     public Account getAccount(Bank bank, String player) throws DAOException;
     public Account getAccount(Bank bank, int accNumber) throws DAOException;
+    public void setPin(Account acct, String pin) throws DAOException;
     
     public void createAccount(Account acct, String player, long amount) throws DAOException;
     public void deposit(Bank bank, Account acct, long amount) throws DAOException;
