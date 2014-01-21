922abeeeffd6cb4b7b45a0c3180774037d1b961f
James Sherlock <lDiverse@users.noreply.github.com>
2014-01-21 09:28:12 +0000

    Multiple message targets

--- src/info/tregmine/commands/MsgCommand.java
+++ src/info/tregmine/commands/MsgCommand.java
@@ -47,34 +47,36 @@ public class MsgCommand extends AbstractCommand
             // TODO: Error message...
             return true;
         }
-
-        TregminePlayer recvPlayer = tregmine.getPlayer(recv);
-        if (recvPlayer == null) {
-            player.sendNotification(Notification.COMMAND_FAIL, ChatColor.RED + "No player found by the name of " + args[0]);
-            return true;
-        }
         
-        boolean ignored;
-        try (IContext ctx = tregmine.createContext()) {
-            IPlayerDAO playerDAO = ctx.getPlayerDAO();
-            ignored = playerDAO.doesIgnore(recvPlayer, player);
-        } catch (DAOException e) {
-            throw new RuntimeException(e);
+        String[] receivingPlayers = args[0].split(",");
+        for (String possiblePlayer : receivingPlayers) {
+            TregminePlayer receivingPlayer = tregmine.getPlayer(possiblePlayer);
+            if (receivingPlayer == null) {
+                player.sendNotification(Notification.COMMAND_FAIL, ChatColor.RED + "No player found by the name of " + args[0]);
+                continue;
+            }
+            
+            boolean ignored;
+            try (IContext ctx = tregmine.createContext()) {
+                IPlayerDAO playerDAO = ctx.getPlayerDAO();
+                ignored = playerDAO.doesIgnore(receivingPlayer, player);
+            } catch (DAOException e) {
+                throw new RuntimeException(e);
+            }
+            if (player.getRank().canNotBeIgnored()) ignored = false;
+            if (ignored) continue;
+            
+            // Show message in senders terminal, as long as the recipient isn't
+            // invisible, to prevent /msg from giving away hidden players presence
+            if (!receivingPlayer.hasFlag(TregminePlayer.Flags.INVISIBLE) || player.getRank().canSeeHiddenInfo()) {
+                player.sendMessage(GREEN + "(to) " + receivingPlayer.getChatName()
+                        + GREEN + ": " + message);
+            }
+            
+            // Send message to recipient
+            receivingPlayer.sendNotification(Notification.MESSAGE, GREEN + "(msg) " + player.getChatName() + GREEN
+                    + ": " + message);
         }
-        if (player.getRank().canNotBeIgnored()) ignored = false;
-        if (ignored == true) return true;
-
-        // Show message in senders terminal, as long as the recipient isn't
-        // invisible, to prevent /msg from giving away hidden players presence
-        if (!recvPlayer.hasFlag(TregminePlayer.Flags.INVISIBLE) || player.getRank().canSeeHiddenInfo()) {
-            player.sendMessage(GREEN + "(to) " + recvPlayer.getChatName()
-                    + GREEN + ": " + message);
-        }
-
-        // Send message to recipient
-        recvPlayer.sendNotification(Notification.MESSAGE, GREEN + "(msg) " + player.getChatName() + GREEN
-                + ": " + message);
-
         return true;
     }
 }
