f78a06d54746739b144e82ba174653adc85d8338
James Sherlock <lDiverse@users.noreply.github.com>
2014-02-07 22:52:42 +0000

    Quit Message

--- src/info/tregmine/listeners/TregminePlayerListener.java
+++ src/info/tregmine/listeners/TregminePlayerListener.java
@@ -397,7 +397,7 @@ public class TregminePlayerListener implements Listener
             } else {
                 Random rand = new Random();
                 int msgIndex = rand.nextInt(plugin.getQuitMessages().size());
-                message = String.format(plugin.getQuitMessages().get(msgIndex), player.getChatName());
+                message = ChatColor.GRAY + "Quit: " + player.getChatName() + " " + plugin.getQuitMessages().get(msgIndex);
             }
             plugin.getServer().broadcastMessage(message);
         }
