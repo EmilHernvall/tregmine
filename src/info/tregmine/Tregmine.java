dd011dba4e98967ff5b49d868672cc421b24df12
James Sherlock <lDiverse@users.noreply.github.com>
2014-02-10 10:09:12 +0000

    _should_ handle nether & end in the anarchy world

--- src/info/tregmine/Tregmine.java
+++ src/info/tregmine/Tregmine.java
@@ -119,6 +119,14 @@ public class Tregmine extends JavaPlugin
         WorldCreator hierarchyWorld = new WorldCreator(config.getString("world.name"));
         hierarchyWorld.environment(World.Environment.NORMAL);
         rulelessWorld = hierarchyWorld.createWorld();
+        
+        // Minecraft Portals should handle these
+        WorldCreator hierarchyWorldNether = new WorldCreator(config.getString("world.name") + "_nether");
+        hierarchyWorldNether.environment(World.Environment.NETHER);
+        hierarchyWorldNether.createWorld();
+        WorldCreator hierarchyWorldEnd = new WorldCreator(config.getString("world.name") + "_the_end");
+        hierarchyWorldEnd.environment(World.Environment.THE_END);
+        hierarchyWorldEnd.createWorld();
 
         try (IContext ctx = contextFactory.createContext()) {
             IBlessedBlockDAO blessedBlockDAO = ctx.getBlessedBlockDAO();
