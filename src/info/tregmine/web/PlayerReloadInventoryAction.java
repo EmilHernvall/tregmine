63d60266b4bcf6947c7132cf985d5e0859b48fec
James Sherlock <lDiverse@users.noreply.github.com>
2014-02-19 16:15:25 +0000

    Create PlayerReloadInventoryAction.java

---
+++ src/info/tregmine/web/PlayerReloadInventoryAction.java
@@ -0,0 +1,105 @@
+package info.tregmine.web;
+
+import static org.bukkit.ChatColor.AQUA;
+
+import java.io.PrintWriter;
+
+import org.bukkit.Server;
+import org.eclipse.jetty.server.Request;
+import org.json.*;
+
+import info.tregmine.*;
+import info.tregmine.api.TregminePlayer;
+
+public class PlayerReloadInventoryAction implements WebHandler.Action
+{
+    public static class Factory implements WebHandler.ActionFactory
+    {
+        public Factory()
+        {
+        }
+
+        @Override
+        public String getName()
+        {
+            return "/reloadinventory";
+        }
+
+        @Override
+        public WebHandler.Action createAction(Request request)
+        throws WebHandler.WebException
+        {
+            try {
+                int subjectId = Integer.parseInt(request.getParameter("subjectId"));
+                int issuerId = Integer.parseInt(request.getParameter("issuerId"));
+                return new PlayerReloadInventoryAction(subjectId, issuerId);
+            }
+            catch (NullPointerException e) {
+                throw new WebHandler.WebException(e);
+            }
+            catch (NumberFormatException e) {
+                throw new WebHandler.WebException(e);
+            }
+        }
+    }
+
+    private int subjectId;
+    private int issuerId;
+
+    private boolean status;
+    private String error;
+
+    public PlayerReloadInventoryAction(int subjectId, int issuerId)
+    {
+        this.subjectId = subjectId;
+        this.issuerId = issuerId;
+
+        this.status = true;
+        this.error = null;
+    }
+
+    @Override
+    public void queryGameState(Tregmine tregmine)
+    {
+        TregminePlayer subject = tregmine.getPlayerOffline(subjectId);
+        if (subject == null) {
+            status = false;
+            error = "Subject not found.";
+            return;
+        }
+
+        TregminePlayer issuer = tregmine.getPlayer(issuerId);
+        if (issuer == null) {
+            issuer = tregmine.getPlayerOffline(issuerId);
+        }
+        
+        if (subject.isOnline()) {
+            subject.loadInventory(subject.getCurrentInventory(), false);
+        }
+        
+        Tregmine.LOGGER.info(subject.getChatName() + "'s inventory was reloaded by " +
+                             issuer.getChatName() + " (from web)");
+
+        Server server = tregmine.getServer();
+        server.broadcastMessage(issuer.getChatName() + AQUA + " reloaded "
+                + subject.getChatName() + AQUA + "'s inventory");
+    }
+
+    @Override
+    public void generateResponse(PrintWriter writer)
+    throws WebHandler.WebException
+    {
+        try {
+            JSONWriter json = new JSONWriter(writer);
+            json.object()
+                .key("status").value(status ? "ok" : "error")
+                .key("error").value(error)
+                .endObject();
+
+            writer.close();
+        }
+        catch (JSONException e) {
+            throw new WebHandler.WebException(e);
+        }
+    }
+}
