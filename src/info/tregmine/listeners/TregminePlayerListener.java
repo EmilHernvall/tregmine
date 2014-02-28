45eb0a578a333469cba28bc91d70f32c0a6a8b73
James Sherlock <lDiverse@users.noreply.github.com>
2014-02-28 10:09:36 +0000

    Update TregminePlayerListener.java

--- src/info/tregmine/listeners/TregminePlayerListener.java
+++ src/info/tregmine/listeners/TregminePlayerListener.java
@@ -512,6 +512,8 @@ public class TregminePlayerListener implements Listener
                 (!player.getRank().canBypassWorld() && player.getGameMode() != GameMode.CREATIVE)) {
             player.setAllowFlight(false);
             player.setFlying(false);
+            event.setCancelled(true);
+            return;
         }
         
         ZoneWorld world = plugin.getWorld(loc.getWorld());
