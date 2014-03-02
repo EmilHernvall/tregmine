bae4964f3f5dadfb8c47e41f76f74c85b0e45f66
Emil Hernvall <emil@znaptag.com>
2014-03-02 21:08:50 +0100

    Various fixes

--- src/info/tregmine/commands/FlyCommand.java
+++ src/info/tregmine/commands/FlyCommand.java
@@ -10,32 +10,33 @@ import info.tregmine.database.IPlayerDAO;
 
 public class FlyCommand extends AbstractCommand
 {
-	private Tregmine tregmine;
+    private Tregmine tregmine;
     public FlyCommand(Tregmine tregmine)
     {
         super(tregmine, "fly");
-		this.tregmine = tregmine;
+        this.tregmine = tregmine;
     }
-    
+
     @Override
     public boolean handlePlayer(TregminePlayer player, String args[])
     {
-        if(!player.getRank().canFly()) return false;
+        if (!player.getRank().canFly()) return false;
 
-		if (	(player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessWorld().getName()) ||
-				player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessEnd().getName()) ||
-				player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessNether().getName())) &&
-				player.getRank().canBypassWorld()) {
-			player.sendMessage(ChatColor.RED + "Flying in Anarchy will get you banned!" + ChatColor.DARK_RED + " Disabled.");
+        if ((player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessWorld().getName()) ||
+             player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessEnd().getName()) ||
+             player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessNether().getName())) &&
+            !player.getRank().canBypassWorld()) {
 
-			for (TregminePlayer p : tregmine.getOnlinePlayers()) {
-				if (p.getRank().canBypassWorld()) {
-					p.sendMessage(player.getChatName() + ChatColor.YELLOW + " attempted to fly in anarchy! Plugin disabled it for you...");
-				}
-			}
+            player.sendMessage(ChatColor.RED + "Flying in Anarchy will get you banned!" + ChatColor.DARK_RED + " Disabled.");
 
-			return false;
-		}
+            for (TregminePlayer p : tregmine.getOnlinePlayers()) {
+                if (p.getRank().canBypassWorld()) {
+                    p.sendMessage(player.getChatName() + ChatColor.YELLOW + " attempted to fly in anarchy! Plugin disabled it for you...");
+                }
+            }
+
+            return false;
+        }
 
         if ( player.hasFlag(TregminePlayer.Flags.FLY_ENABLED) ) {
             player.sendMessage(ChatColor.YELLOW +
@@ -44,19 +45,19 @@ public class FlyCommand extends AbstractCommand
             player.setAllowFlight(false);
         } else {
 
-            player.sendMessage(ChatColor.YELLOW + 
+            player.sendMessage(ChatColor.YELLOW +
                     "Flying Enabled!");
             player.setFlag(TregminePlayer.Flags.FLY_ENABLED);
             player.setAllowFlight(true);
         }
-        
+
         try (IContext ctx = tregmine.createContext()) {
             IPlayerDAO playerDAO = ctx.getPlayerDAO();
             playerDAO.updatePlayer(player);
         } catch (DAOException e) {
             throw new RuntimeException(e);
         }
-        
+
         return true;
     }
 }
