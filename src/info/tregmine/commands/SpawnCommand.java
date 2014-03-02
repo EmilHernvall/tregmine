5bffbbfaede6c8afbe784b7fe8f1999ea5d4cafb
James <jamez.s.97@gmail.com>
2014-03-02 13:22:45 +0000

    Added 30 second delay on Spawn in Anarchy

--- src/info/tregmine/commands/SpawnCommand.java
+++ src/info/tregmine/commands/SpawnCommand.java
@@ -1,20 +1,66 @@
 package info.tregmine.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Horse;
 
 import info.tregmine.Tregmine;
 import info.tregmine.api.TregminePlayer;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import static org.bukkit.ChatColor.DARK_RED;
+import static org.bukkit.ChatColor.RED;
 
 public class SpawnCommand extends AbstractCommand
 {
+	private static class SpawnTask implements Runnable
+	{
+		private TregminePlayer player;
+		private Location loc;
+		private Location oldLocation;
+
+		public SpawnTask(TregminePlayer player, Location loc)
+		{
+			this.player = player;
+			this.loc = loc;
+			this.oldLocation = player.getLocation();
+		}
+
+		@Override
+		public void run()
+		{
+			if (oldLocation != player.getLocation()) {
+				player.sendMessage(ChatColor.RED + "Teleportation stopped! You moved...");
+				return;
+			}
+			player.teleportWithHorse(loc);
+		}
+	}
+
+	private Tregmine plugin;
     public SpawnCommand(Tregmine tregmine)
     {
         super(tregmine, "spawn");
+		this.plugin = tregmine;
     }
 
     @Override
     public boolean handlePlayer(TregminePlayer player, String[] args)
     {
+		if (	player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessWorld().getName()) ||
+				player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessEnd().getName()) ||
+				player.getWorld().getName().equalsIgnoreCase(tregmine.getRulelessNether().getName()) &&
+						!player.getRank().canBypassWorld()) {
+
+			player.sendMessage(ChatColor.RED + "Teleporting to spawn... Please wait 30 seconds.");
+			player.sendMessage(ChatColor.DARK_BLUE + "Oh, and don't move! Moving will stop the teleportation.");
+			BukkitScheduler scheduler = plugin.getServer().getScheduler();
+			scheduler.scheduleSyncDelayedTask(tregmine, new SpawnTask(player, player.getWorld().getSpawnLocation()), 20*30);
+			return true;
+		}
         player.teleportWithHorse(player.getWorld().getSpawnLocation());
         return true;
     }
